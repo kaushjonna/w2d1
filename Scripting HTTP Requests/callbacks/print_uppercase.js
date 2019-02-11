54e2701ea6c75486f327fbf6d41db83d93067955
Kaush <kaush.jonna@gmail.com>
2019-02-11 13:16:00 -0500

    completed callback funcitons except print_1337.js

---
+++ Scripting HTTP Requests/callbacks/print_uppercase.js
@@ -0,0 +1,14 @@
+const getHTML = require('./http-functions');
+
+const requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step6/uppercase.html',
+};
+
+function printUpperCase(html) {
+  html.forEach((element) => {
+    console.log(`${element} \n`.toUpperCase());
+  });
+}
+
+getHTML(requestOptions, printUpperCase);
